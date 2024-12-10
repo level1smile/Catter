8b6e003290a39667ce7696ebdda5a7fdfb799e1e
NicoMico <Administrator@DESKTOP>
2024-12-10 13:15:52 +0800

    op

--- core/func_operators.py
+++ core/func_operators.py
@@ -1,4 +1,12 @@
-# 目前共设计三个接口：
-# 1.自动检测使用GPU-PreSkinning技术的DrawIB列表
-# 2.提取模型
-# 3.按Buffer导出并直接生成二创模型（跳过导出模型步骤）
+# 直接导出生成Mod
+import bpy
+
+class DBMTExportMergedModVBModel(bpy.types.Operator):
+    bl_idname = "dbmt.generate_mod"
+    bl_label = "DBMTGenerateMod"
+    bl_description = "Directly generate mod files from blender model"
+
+    def execute(self, context):
+
+
+         return {'FINISHED'}
